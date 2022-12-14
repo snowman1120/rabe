fdfb346f3671bf0bc1a87e9b396ae4d78870da9e
snowman1120 <snowman19901120@gmail.com>
2022-12-14 10:20:07 -0500

    Client build

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ process.env.NODE_ENV = "production";
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('/client/build'));
-  app.use(express.static('/admin/build'));
+  app.use(express.static('client/build'));
+  app.use(express.static('admin/build'));
 
   app.get('/admin/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'admin', 'build', 'index.html'));
