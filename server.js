268e673ed40c02ec0bd7fdf0c71ad6d8ed2d39ae
snowman1120 <snowman19901120@gmail.com>
2022-12-15 21:09:44 -0500

    Start server on 80 port

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 http.listen(PORT, () => console.log(`Server started on port ${PORT}`));
